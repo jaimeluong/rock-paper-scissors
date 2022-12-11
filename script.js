06c762c4bbb94db742ca71233abd12a3cb1639da
Jaime Luong <jaime.luong@gmail.com>
2022-12-11 16:50:27 -0600

    Finish refactoring to incorporate UI

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // Get nodes on page
+// Rock, paper, scissors buttons
 const rockButton = document.querySelector('#rockBtn'); // Rock button
 const paperButton = document.querySelector('#paperBtn'); // Paper button
 const scissorsButton = document.querySelector('#scissorsBtn'); // Scissors button
 
-const choiceMade = document.querySelector('.choiceMade'); // Line of text saying the player's most recent choice
+// Text showing the choice the player just made
+const choiceMade = document.querySelector('.choiceMade');
+choiceMade.textContent = 'You chose...';
 
-const playerRunningScore = document.querySelector('.playerRunningScore'); // Number showing the player's current score
+// Running scores for player and computer
+const playerRunningScore = document.querySelector('.playerRunningScore');
 playerRunningScore.textContent = 0;
-const computerRunningScore = document.querySelector('.computerRunningScore'); // Number showing the computer's current score
+const computerRunningScore = document.querySelector('.computerRunningScore');
 computerRunningScore.textContent = 0;
 
-const roundWinner = document.querySelector('.roundWinner'); // Line of text saying who won that round
-const finalWinner = document.querySelector('.finalWinner'); // Line of text displaying the final winner
+// Text showing who won the last round
+const roundWinner = document.querySelector('.roundWinner');
+// Text showing the final winner
+const finalWinner = document.querySelector('.finalWinner')
 
-// Array of possible answers
+// Array of possible choices
 let choices = ['rock', 'paper', 'scissors'];
 
 // Generate computer choice
@@ -22,47 +28,103 @@ const getComputerChoice = () => {
     let num = Math.floor(Math.random() * 3 + 1);
     let computerChoice;
     switch(num) {
-        case 1: computerChoice = choices[0];
+        case 1: computerChoice = choices[0]; // Rock
             break;
-        case 2: computerChoice = choices[1];
+        case 2: computerChoice = choices[1]; // Paper
             break;
-        case 3: computerChoice = choices[2];
+        case 3: computerChoice = choices[2]; // Scissors
             break;
     }
     return computerChoice;
 }
 
-// Array of computer's choices to play against
-const cpuChoices = [];
-for(var i=0; i<5; i++) {
-    cpuChoices.push(getComputerChoice());
-}
-console.log(cpuChoices);
-
-// Add event listeners to 3 buttons
-rockButton.addEventListener('click', () => {
-    playGame('rock');
-});
-paperButton.addEventListener('click', () => {
-    playGame('paper');
-});
-scissorsButton.addEventListener('click', () => {
-    playGame('scissors');
-});
-
 // Initialize counter variables
 let playerScore = 0;
 let cpuScore = 0;
+let rounds = 0;
+
+// Add event listeners to 3 buttons
+// On click, run playGame with the specifed choice
+// Stop after 5 rounds and declare winner
+rockButton.addEventListener('click', () => {
+    playGame('rock');
+    rounds++;
+    if(rounds === 5) {
+        if(playerScore > cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you win!`;
+        } else if(playerScore < cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you lose!`;
+        } else {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you tie!`;
+        }
+    }
+});
+paperButton.addEventListener('click', () => {
+    playGame('paper');
+    rounds++;
+    if(rounds === 5) {
+        if(playerScore > cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you win!`;
+        } else if(playerScore < cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you lose!`;
+        } else {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you tie!`;
+        }
+    }
+});
+scissorsButton.addEventListener('click', () => {
+    playGame('scissors');
+    rounds++;
+    if(rounds === 5) {
+        if(playerScore > cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you win!`;
+        } else if(playerScore < cpuScore) {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you lose!`;
+        } else {
+            finalWinner.textContent = `Player won ${playerScore} round(s) and computer won ${cpuScore} round(s), so you tie!`;
+        }
+    }
+});
+
+// Update running score display
+const updateRunningScore = () => {
+    playerRunningScore.textContent = playerScore;
+    computerRunningScore.textContent = cpuScore;
+}
 
 // Play a game
-const playGame = (choice) => { // Choice is a string
-    choiceMade.textContent = `You chose ${choice}`;
-    if(choice === cpuChoices[0]) { // If a tie is found for the first round, works for R/P/S
-        roundWinner.textContent = `Computer and you both chose ${choice}, so you've tied`;
-    } else if(choice === 'rock' && cpuChoices[0] === 'scissors') {
-        roundWinner.textContent = `Rock beats scissors, so you win`;
-        playerScore++;
+const playGame = (playerChoice) => { // Choice is a string
+    choiceMade.textContent = `You chose ${playerChoice}`;
+    let computerChoice = getComputerChoice();
+
+    if(playerChoice === computerChoice) { // Tie condition
+        roundWinner.textContent = `You both chose ${playerChoice}, so you tie!`;
+        updateRunningScore();
     } else {
-        roundWinner.textContent = `Scissors beats rock, so you lost`;
+        if(playerChoice === choices[0] && computerChoice === 'paper') { // Player chose rock, computer chose paper
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you lose!`;
+            cpuScore++;
+            updateRunningScore();
+        } else if(playerChoice === choices[0] && computerChoice === 'scissors') { // Player chose rock, computer chose scissors
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you win!`;
+            playerScore++;
+            updateRunningScore();
+        } else if(playerChoice === choices[1] && computerChoice === 'rock') { // Player chose paper, computer chose rock
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you win!`;
+            playerScore++;
+            updateRunningScore();
+        } else if(playerChoice === choices[1] && computerChoice === 'scissors') { // Player chose paper, computer chose scissors
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you lose!`;
+            cpuScore++;
+            updateRunningScore();
+        } else if(playerChoice === choices[2] && computerChoice === 'rock') { // Player chose scissors, computer chose rock
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you lose!`;
+            cpuScore++;
+            updateRunningScore();
+        } else { // Final case for playing chose scissors and computer chose paper
+            roundWinner.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}, so you win!`;
+            playerScore++;
+            updateRunningScore();
+        }
     }
 }
